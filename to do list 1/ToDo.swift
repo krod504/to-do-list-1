c625fe422afa680ed364cbb97a25418b1d2b8a18
krod504 <krod3584@gmail.com>
2020-06-08 13:49:55 -0400

    code added

---
+++ to do list 1/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  to do list 1
+//
+//  Created by Karla Rodriguez on 6/8/20.
+//  Copyright © 2020 Karla Rodriguez. All rights reserved.
+//
+
+import  UIKit
+class ToDo{
+    var name = ""
+    var important = false
+}
+
+
