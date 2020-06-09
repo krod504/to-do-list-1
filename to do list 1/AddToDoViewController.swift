ece8967f7eb8393bcd3c56cf8fe13a719658a892
krod504 <krod3584@gmail.com>
2020-06-09 10:40:34 -0400

    Complete Iteration 2

--- to do list 1/AddToDoViewController.swift
+++ to do list 1/AddToDoViewController.swift
@@ -20,18 +20,36 @@ class AddToDoViewController: UIViewController {
       }
   
     @IBAction func addTapped(_ sender: Any) {
-    let toDo = ToDo()
-               
+    
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+
+            // we are creating a new ToDoCD object here, naming it toDo
+            let toDo = ToDoCD(entity: ToDoCD.entity(), insertInto: context)
+
+            // if the titleTextField has text, we will call that text titleText
             if let titleText = titleTextField.text {
-                 toDo.name = titleText
+                // we will take the titleText and assign that value to toDo.name
+                // this .name and .important came from the attributes you typed in on the Core Data page!
+                toDo.name = titleText
                 toDo.important = importantSwitch.isOn
-               }
-               previousVC.toDos.append(toDo)
-              previousVC.tableView.reloadData()
-               navigationController?.popViewController(animated: true)
+            }
+
+            try? context.save()
+
+            navigationController?.popViewController(animated: true)
+          }
+
         }
-
-
+    
+    // let toDo = ToDo()
+               //if let titleText = titleTextField.text {
+                   // toDo.name = titleText
+                 //  toDo.important = importantSwitch.isOn
+                  //}
+                //  previousVC.toDos.append(toDo)
+            //     previousVC.tableView.reloadData()
+               //   navigationController?.popViewController(animated: true)
+       //    }
 
     /*
     // MARK: - Navigation
--- to do list 1/ToDoTableTableViewController.swift
+++ to do list 1/ToDoTableTableViewController.swift
@@ -9,26 +9,46 @@
 import UIKit
 
 class ToDoTableTableViewController: UITableViewController {
+   
+    var toDos : [ToDoCD] = []
     
-   func createToDos() -> [ToDo] {
+    override func viewWillAppear(_ animated: Bool) {
+      getToDos()
+    }
+   
+   // func createToDos() -> [ToDo] {
+      //  let swift = ToDo()
+   //  swift.name = "Learn Swift"
+   //  swift.important = true
 
-     let swift = ToDo()
-     swift.name = "Learn Swift"
-     swift.important = true
-
-     let dog = ToDo()
-     dog.name = "Walk the Dog"
+   //  let dog = ToDo()
+  //   dog.name = "Walk the Dog"
      // important is set to false by default
 
-     return [swift, dog]
-   }
-    var toDos : [ToDo] = []
+   //  return [swift, dog]
+ //  }
+    func getToDos() {
+      if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
 
+      if let coreDataToDos = try? context.fetch(ToDoCD.fetchRequest()) as? [ToDoCD] {
+                 toDos = coreDataToDos
+                 tableView.reloadData()
+         }
+       }
+     }
+    
+    
+    
+    
+    
+    
+    
+    
+   // var toDos : [ToDo] = []
+   //  toDos = createToDos()
     override func viewDidLoad() {
-        toDos = createToDos()
         super.viewDidLoad()
         
-
     }
 
 
@@ -57,16 +77,26 @@ class ToDoTableTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
         
         let toDo = toDos[indexPath.row]
-        
-        if toDo.important {
-          cell.textLabel?.text = "❗️" + toDo.name
-        } else {
-          cell.textLabel?.text = toDo.name
+        if let name = toDo.name {
+            if toDo.important {
+                cell.textLabel?.text = "❗️" + name
+            } else {
+                cell.textLabel?.text = toDo.name
+            }
+          }
+
+          return cell
         }
+    
+        //if toDo.important {
+       //   cell.textLabel?.text = "❗️" + toDo.name
+     //   } else {
+      //    cell.textLabel?.text = toDo.name
+       // }
         // Configure the cell...
         
-        return cell
-    }
+      //  return cell
+ //   }
     
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -123,7 +153,7 @@ class ToDoTableTableViewController: UITableViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
          if let completeVC = segue.destination as? CompleteToDoViewController {
-            if let toDo = sender as? ToDo {
+            if let toDo = sender as? ToDoCD {
               completeVC.selectedToDo = toDo
               completeVC.previousVC = self
             }
